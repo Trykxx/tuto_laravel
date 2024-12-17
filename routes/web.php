cde5c5e5a7b816dcf1aea3f0a9c425f4dd533647
Trykxx <amitnicolas@gmail.com>
2024-12-17 17:52:49 +0100

    add db features

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -9,6 +10,17 @@ Route::get('/', function () {
 
 Route::prefix('/blog')->name('blog.')->group(function(){
     Route::get('/', function () {
+
+        $post = new Post();
+
+        $post->title = "Mon second article";
+        $post->slug = "Mon-second-article";
+        $post->content = 'Mon contenu';
+
+        $post->save();
+
+        return $post;
+
         return [
             "link" => \route('blog.show', ['slug' => 'article', 'id'=> 15])
         ];
